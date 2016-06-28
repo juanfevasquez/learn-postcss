9e053d3af55edf8adb9dbaa396b682d967ae2349
XumaK S.A.S Medellin <xumak@XumaKs-MacBook-Pro.local>
2016-06-28 11:28:03 -0500

    Prepares files for Demo

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,7 @@ var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
 var sync = require('browser-sync');
 var postcss = require('gulp-postcss');
-var cssnano = require('cssnano');
-var cssnext = require('cssnext');
 var autoprefixer = require('autoprefixer');
-var lost = require('lost');
-var sprites = require('postcss-sprites').default;
-var stylelint = require('stylelint');
-var fs = require('fs');
 
 gulp.task('sass', function() {
   gulp.src('source/scss/main.scss')
