cb876db002f91337184a5049624f2dba979906b6
XumaK S.A.S Medellin <xumak@XumaKs-MacBook-Pro.local>
2016-06-28 11:36:46 -0500

    Prepares files for Demo

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,7 @@ var maps = require('gulp-sourcemaps');
 var sync = require('browser-sync');
 var postcss = require('gulp-postcss');
 var cssnano = require('cssnano');
-var cssnext = require('cssnext');
 var autoprefixer = require('autoprefixer');
-var lost = require('lost');
-var sprites = require('postcss-sprites').default;
-var stylelint = require('stylelint');
-var fs = require('fs');
 
 gulp.task('sass', function() {
   gulp.src('source/scss/main.scss')
@@ -18,6 +13,7 @@ gulp.task('sass', function() {
   .pipe(sass().on('error', sass.logError))
   .pipe(maps.write())
   .pipe(postcss([
+      autoprefixer(),
       cssnano({discardComments: { removeAll: true }})
     ]))
   .on('error', gutil.log)
